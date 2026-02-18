a28faf1b89530f239dfeca98a836be06002860a1
vaishnavijagtap02 <vaishjagtap02@gmail.com>
2026-02-18 16:11:49 +0530

    UC2:Hardcoded palindrome

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -15,4 +15,36 @@ public class UseCasePalindromeCheckerApp {
     }
 }
 
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        System.out.println("Palindrome Checker App - UC2: Hardcoded Palindrome Result");
+        System.out.println("Checking the word: " + word);
+
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is NOT a palindrome.");
+        }
+    }
+
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+}
+
+
 
