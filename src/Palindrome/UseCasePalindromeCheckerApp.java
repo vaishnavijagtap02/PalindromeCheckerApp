e0b3656afda38362bc1e9fcb9baadc82e64d584b
vaishnavijagtap02 <vaishjagtap02@gmail.com>
2026-02-18 15:58:19 +0530

    Base application setup

---
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+package Palindrome;
+
+public class UseCasePalindromeCheckerApp {
+  public   static void main(String[] args) {
+
+      String appName = "Palindrome Checker App";
+      String appVersion = "1.0.0";
+
+      System.out.println("----------------------------------------");
+      System.out.println("Welcome to the " + appName);
+      System.out.println("Application Version: " + appVersion);
+      System.out.println("----------------------------------------");
+
+      System.out.println("System initialized successfully.");
+    }
+}
+
+
